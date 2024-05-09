683ada38d8429c80a10bfd090e0646d955e67cb1
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-05-09 08:03:58 +0200

    Add support for flask projects

--- orchestration/src/FizzyLogic.Aspire.Python.Hosting/FlaskProjectResourceBuilderExtensions.cs
+++ orchestration/src/FizzyLogic.Aspire.Python.Hosting/FlaskProjectResourceBuilderExtensions.cs
@@ -1,38 +1,32 @@
-using System.Collections;
-using Aspire.Hosting;
+﻿using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
-using Aspire.Hosting.Publishing;
 
 namespace FizzyLogic.Aspire.Python.Hosting;
 
-public static class PythonProjectResourceBuilderExtensions
+public static class FlaskProjectResourceBuilderExtensions
 {
     /// <summary>
-    /// Adds a python project that's built with a virtual environment to the distributed application. We assume that you
-    /// have a Dockerfile in your project directory and that the virtual environment is located in the .venv directory.
+    /// Adds a flask service to the distributed application
     /// </summary>
-    /// <param name="builder"></param>
-    /// <param name="name">The name of the python project</param>
-    /// <param name="projectDirectory">The directory where the project files are located</param>
-    /// <param name="entrypoint">Script to run when the project is started</param>
-    /// <param name="args">Extra command line arguments for the project</param>
-    /// <returns>Returns the resource builder</returns>
-    public static IResourceBuilder<PythonProjectResource> AddPythonProjectWithVirtualEnvironment(
-        this IDistributedApplicationBuilder builder, string name, string projectDirectory,
-        string entrypoint = "main.py", params string[]? args)
+    /// <param name="builder">Distributed application builder instance</param>
+    /// <param name="name">The name of the service</param>
+    /// <param name="projectDirectory">The directory containing the project</param>
+    /// <param name="entrypoint">The script containing the flask app variable</param>
+    /// <returns>Returns the resource builder for further extension</returns>
+    public static IResourceBuilder<FlaskProjectResource> AddFlaskProjectWithVirtualEnvironment(this IDistributedApplicationBuilder builder, string name, string projectDirectory, string entrypoint = "main.py")
     {
         var absoluteProjectDirectory = Path.GetFullPath(Path.Join(builder.AppHostDirectory, projectDirectory));
         var virtualEnvironment = new VirtualEnvironment(absoluteProjectDirectory);
         var instrumentationExecutable = virtualEnvironment.GetExecutable("opentelemetry-instrument");
-        var pythonExecutable = virtualEnvironment.GetExecutable("python");
-        var projectExecutable = instrumentationExecutable ?? pythonExecutable!;
-        var projectResource = new PythonProjectResource(name, projectExecutable, absoluteProjectDirectory);
-        
+        var flaskExecutable = virtualEnvironment.GetExecutable("flask");
+        var projectExecutable = instrumentationExecutable ?? flaskExecutable!;
+        var projectResource = new FlaskProjectResource(name, projectExecutable, absoluteProjectDirectory);
+
         var resourceBuilder = builder.AddResource(projectResource).WithArgs(context =>
         {
             if (!string.IsNullOrEmpty(instrumentationExecutable))
             {
-                // // Export the logs to the OTLP endpoint only. We already have logging.
+                // Export the logs to the OTLP endpoint only. We already have logging.
                 context.Args.Add("--traces_exporter");
                 context.Args.Add("otlp");
 
@@ -41,23 +35,20 @@ public static class PythonProjectResourceBuilderExtensions
 
                 context.Args.Add("--metrics_exporter");
                 context.Args.Add("otlp");
-
-                context.Args.Add(pythonExecutable!);
+                
+                context.Args.Add(flaskExecutable!);
             }
 
             // Always include the entrypoint script as the first argument
-            context.Args.Add(entrypoint);
-
-            if (args is not null)
-            {
-                // Next include the provided set of arguments
-                foreach (var arg in args)
-                {
-                    context.Args.Add(arg);
-                }
-            }
+            context.Args.Add("--app");
+            context.Args.Add(Path.GetFileNameWithoutExtension(entrypoint));
+            context.Args.Add("run");
         });
-
+        
+        // Automatically add the HTTP endpoint for the Flask project (which in this case is always on port 5000)
+        // Expose the port on the FLASK_RUN_PORT to allow the Flask app to bind to the correct port.
+        resourceBuilder.WithHttpEndpoint(targetPort: 5000, name: "http", env: "FLASK_RUN_PORT");
+        
         // Make sure to wire up the OTLP exporter automatically when we're using opentelemetry instrumentation.
         if (!string.IsNullOrEmpty(instrumentationExecutable))
         {
--- orchestration/src/FizzyLogic.Aspire.Python.Hosting/PythonProjectResource.cs
+++ orchestration/src/FizzyLogic.Aspire.Python.Hosting/PythonProjectResource.cs
@@ -1,5 +1,6 @@
 using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Publishing;
 
 namespace FizzyLogic.Aspire.Python.Hosting;
 
@@ -9,5 +10,28 @@ namespace FizzyLogic.Aspire.Python.Hosting;
 /// <param name="name"></param>
 public class PythonProjectResource(string name, string pythonExecutable, string workingDirectory): ExecutableResource(name, pythonExecutable, workingDirectory),  IResourceWithEnvironment, IResourceWithArgs, IResourceWithServiceDiscovery
 {
-    
+    /// <summary>
+    /// Writes out the publishing manifest for the python project.
+    /// </summary>
+    /// <param name="context"></param>
+    public async Task WriteDockerFileManifestAsync(ManifestPublishingContext context)
+    {
+        var dockerFilePath = Path.Combine(WorkingDirectory, "Dockerfile");
+        var manifestRelativeDockerFilePath = context.GetManifestRelativePath(dockerFilePath);
+        var manifestRelativeWorkingDirectory = context.GetManifestRelativePath(WorkingDirectory);
+
+        if (!File.Exists(dockerFilePath))
+        {
+            throw new InvalidOperationException(
+                "Dockerfile not found in project directory. Please provide a Dockerfile in the project directory.");
+        }
+
+        context.Writer.WriteString("type", "dockerfile.v0");
+        context.Writer.WriteString("path", manifestRelativeDockerFilePath);
+        context.Writer.WriteString("context", manifestRelativeWorkingDirectory);
+
+        await context.WriteEnvironmentVariablesAsync(this).ConfigureAwait(false);
+
+        context.WriteBindings(this);
+    }   
 }
